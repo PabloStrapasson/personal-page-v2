0d45877a93199758f2a77f8ec07101d6e23dd1aa
PabloStrapasson <pablostrapasson98@gmail.com>
2025-03-31 16:28:57 -0300

    feat: :sparkles: Create page for projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,33 +1,33 @@
 import React from 'react'
 
-type Certification = {
+type Project = {
   _id: string;
   name: string;
-  workload: number;
-  conclusion_date: Date;
+  tecnologies: string[];
+  git_repository: string;
   description: string;
-  certificate_link: string;
+  deploy_url?: string;
 }
 
-async function getCertifications() {
-  const response = await fetch(`${process.env.BASE_URL}/api/certifications`, { method: 'GET' });
-  const certifications = await response.json();
-  return certifications;
+async function getProjects() {
+  const response = await fetch(`${process.env.BASE_URL}/api/projects`, { method: 'GET' });
+  const projects = await response.json();
+  return projects;
 }
 
-export default async function Certifications() {
+export default async function Projects() {
 
-  const certifications = await getCertifications()
-  
+  const projects = await getProjects()
+  // console.log(projects)
   return (
     <div className="flex items-center justify-center min-h-screen pt-20 px-2 pb-20 gap-12 md:pt-28 lg:px-20 lg:py-0 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center justify-center sm:items-start snap-y snap-mandatory">
         
         <section id='my_certifications' className='snap-start w-full flex flex-col justify-center'>
-          <h1> Certifications </h1>
+          <h1> Projects </h1>
           <ul>
-            {certifications.map((certification: Certification) => (
-              <li key={certification._id}>{certification.name}</li>
+            {projects.map((project: Project) => (
+              <li key={project._id}>{project.name} {project._id}</li>
             ))}
           </ul>
         </section>
