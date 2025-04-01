b6faebc64157d32c2da28efdc6b5ed429b7ad4d7
PabloStrapasson <pablostrapasson98@gmail.com>
2025-04-01 20:27:10 -0300

    feat: :sparkles: Create the projects section and certifications sections on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import ProfileSection from "@/components/sections/profile-section";
+import ProjectsSection from "@/components/sections/projects-section";
 import TecnologiesSection from '@/components/sections/tecnologies-section';
+import CertificationsSection from '@/components/sections/certifications-section';
 
-export default function Home() {
+export default async function Home() {
+
   return (
     // grid grid-rows-[20px_1fr_20px] items-center justify-center min-h-screen py-8 px-2 lg:px-20 pb-20 gap-12 sm:p-20 font-[family-name:var(--font-geist-sans)]
     <div className="flex items-center justify-center min-h-screen pt-20 px-2 pb-20 gap-12 md:pt-28 lg:px-20 lg:py-0 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center justify-center sm:items-start snap-y snap-mandatory">
         
-        <section id='about_me' className='snap-start w-full flex justify-center'>
+        <section id='about_me' className='snap-start w-full'>
           <ProfileSection/>
         </section>
 
@@ -15,16 +18,21 @@ export default function Home() {
           <TecnologiesSection/>
         </section>
 
+        <br id="projects"/>
+        <br />
+
         <section id='my_projects' className='snap-start w-full'>
-          <ProfileSection/>
+          <ProjectsSection/>
         </section>
 
+        <br id="certifications"/>
+        <br />
+
         <section id='my_certifications' className='snap-start w-full'>
-          <ProfileSection/>
+          <CertificationsSection/>
         </section>
         
       </main>
-      
     </div>
   );
 }
---
+++ src/components/sections/certifications-section.tsx
@@ -0,0 +1,59 @@
+import CertificationLg from "@/components/certification/certifications-lg";
+import Certification from "@/components/certification/certification";
+
+type Certification = {
+  _id: string;
+  name: string;
+  workload: number;
+  conclusion_date: Date;
+  description: string;
+  certificate_link: string;
+}
+  
+async function getCertifications() {
+  const response = await fetch(`${process.env.BASE_URL}/api/certifications`, { method: 'GET' });
+  const certifications = await response.json();
+  return certifications;
+}
+
+export default async function CertificationsSection() {
+
+  const certifications = await getCertifications()
+
+  return (
+    <div className='w-full flex flex-col justify-center items-center mb-5 md:mb-10 lg:mb-25 xl:mb-40'>
+        <div className='w-full px-5 mb-5 flex md:p-5'>
+            <h1 className='text-[14px] font-bold md:text-[18px] lg:text-[30px]'> Cursos e Certificações: </h1>
+        </div>
+
+        <div className='hidden
+                        md:grid md:grid-cols-1 md:gap-10 
+                        lg:grid-cols-1
+                        xl:grid-cols-2 xl:gap-14'>
+            {certifications.map((certification: Certification) => (
+                <CertificationLg key={certification._id}
+                                title={certification.name}
+                                workload={certification.workload}
+                                conclusion_date={certification.conclusion_date}
+                                description={certification.description}
+                                certificate_link={certification.certificate_link}
+                />
+            ))}
+        </div>
+
+        <div className='grid grid-cols-1 gap-4
+                        md:hidden'>  
+            {certifications.map((certification: Certification) => (
+                <Certification key={certification._id}
+                               title={certification.name}
+                               workload={certification.workload}
+                               conclusion_date={certification.conclusion_date}
+                               description={certification.description}
+                               certificate_link={certification.certificate_link}
+                />
+            ))}
+        </div>
+
+    </div>
+  )
+}
