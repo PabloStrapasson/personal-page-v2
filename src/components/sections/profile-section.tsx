9e4914ab71068e60c02dee5688c41ed80fff796e
PabloStrapasson <pablostrapasson98@gmail.com>
2025-04-04 11:46:10 -0300

    refactor: :recycle: Removal of some comments

--- src/components/sections/profile-section.tsx
+++ src/components/sections/profile-section.tsx
@@ -1,8 +1,6 @@
 import About from "@/components/profile/about";
 import ProfilePicture from '@/components/profile/profile-picture';
 
-//flex flex-col w-full gap-5 md:flex-row md:items-center md:justify-between lg:justify-center lg:gap-36 lg:h-screen
-
 export default function ProfileSection() {
   return (
     <div className='flex flex-col w-full gap-5 
